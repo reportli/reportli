9ff3eeb385f74ab6b630959435ab379a3e8d08b5
Shane Afsar <safsar@linkedin.com>
2015-02-20 15:37:25 -0800

    deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PARSE_APP_NAME="Your Parse App Name"
-export PARSE_APP_ID="APP_ID"
-export PARSE_MASTER_KEY="MASTER_KEY"
+export PARSE_APP_NAME="reportli"
+export PARSE_APP_ID="AQ2JzJBrqkne4KsZ3o4XYdG7bHlGPD9rWkepienY"
+export PARSE_MASTER_KEY="AmXnR3vq30rQJ3T7O72O6nXOkpAUYYAREd2DXNk3"
 ember parse deploy
